f4001d03943b5d56bca6e379f7553590f5a3ae69
LD <LDinho@users.noreply.github.com>
2019-03-13 22:54:14 -0400

    update dummy-data file; set up access to data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import dummyData from './dummy-data';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: dummyData,
+
+    };
+
+  };
   render() {
+    const {posts} = this.state;
+    console.log('POSTS DATA:', posts);
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <div>
+            {posts.map((post) => {
+              return <p key={post.id}>{post.username}</p>;
+            })}
+          </div>
+
         </header>
       </div>
     );
